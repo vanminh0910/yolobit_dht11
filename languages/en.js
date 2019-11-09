7e9ad29efab3a6b09d773e860266a31bc259a850
Minh Ha <vanminh0910@gmail.com>
2019-11-09 16:56:43 +0700

    Add block dht_create

---
+++ languages/en.js
@@ -0,0 +1,3 @@
+Blockly.Msg.DHT_MESSAGE0 = "create %1 type %2 pin %3"
+Blockly.Msg.DHT_TOOLTIP = "Create and initiate DHT sensor with specified type and pin"
+Blockly.Msg.DHT_HELPURL = ""
---
+++ languages/vi.js
@@ -0,0 +1,3 @@
+Blockly.Msg.DHT_MESSAGE0 = "tạo %1 loại %2 chân cắm %3"
+Blockly.Msg.DHT_TOOLTIP = "Khởi tạo cảm biến DHT với loại và chân cắm được chọn"
+Blockly.Msg.DHT_HELPURL = ""
